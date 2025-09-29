b544641366a31bcc12949e22a0896618cd02affa
VietAnh1010 <vietanh101003@gmail.com>
2025-09-29 13:49:29 +0800

    Add more syntax

--- example.v
+++ example.v
@@ -9,51 +9,49 @@ Open Scope string_scope.
 Open Scope Z_scope.
 
 Example ex1 :=
-  <{ let "f" =
-         reset begin
-           let "x" = 6 * 9 in
-           let "y" = (shift "k" => "k") in
-           "x" * "y"
-         end
+  <{ let "f" :=
+       reset
+         let "x" := 6 * 9 in
+         let "y" := shift "k" "k" in
+         "x" * "y"
      in
      "f" 10 }>.
 
 Compute (eval_term 2 ex1).
 
 Example append_aux :=
-  <{ fix "append_aux" "xs" =>
+  <{ fix "append_aux" "xs" :=
        match "xs" with
-       | Inl {BAnon} => shift "k" => "k"
+       | Inl _ => shift "k" "k"
        | Inr "xs" =>
-           let "x", "xs'" = "xs" in
-           let "r" = "append_aux" "xs'" in
-           let "r" = "x", "r" in
+           let ("x", "xs'") := "xs" in
+           let "r" := "append_aux" "xs'" in
+           let "r" := ("x", "r") in
            Inr "r"
        end }>.
 
 Example append :=
-  <{ let "append_aux" = {append_aux} in
+  <{ let "append_aux" := append_aux in
      fun "xs" => reset "append_aux" "xs" }>.
 
 Fixpoint encode (xs : list Z) :=
   match xs with
   | [] => <{ Inl () }>
-  | x :: xs' =>
-      <{ let "xs'" = {encode xs'} in
-         let "xs" = {x}, "xs'" in
-         Inr "xs" }>
+  | x :: xs' => <{ let "xs'" := {encode xs'} in
+                   let "xs" := (x, "xs'") in
+                   Inr "xs" }>
   end.
 
 Example append1 xs :=
-  <{ let "append" = {append} in
-     let "xs" = {xs} in
+  <{ let "append" := append in
+     let "xs" := xs in
      "append" "xs" }>.
 
 Example append2 xs ys :=
-  <{ let "append" = {append} in
-     let "xs" = {xs} in
-     let "ys" = {ys} in
-     let "append1" = "append" "xs" in
+  <{ let "append" := append in
+     let "xs" := xs in
+     let "ys" := ys in
+     let "append1" := "append" "xs" in
      "append1" "ys" }>.
 
 Compute (eval_term 3 (append1 (encode []))).
@@ -64,31 +62,31 @@ Compute (eval_term 4 (append2 (encode [1]) (encode [2]))).
 Fixpoint sequence start len : list Z :=
   match len with
   | O => []
-  | S len' => start :: sequence (Z.succ start) len'
+  | S len' => start :: sequence (start + 1) len'
   end.
 
 Example append_direct :=
-  <{ fix "append_direct" "xs" "ys" =>
+  <{ fix "append_direct" "xs" "ys" :=
        match "xs" with
-       | Inl {BAnon} => "ys"
+       | Inl _ => "ys"
        | Inr "xs" =>
-           let "x", "xs'" = "xs" in
-           let "append_direct1" = "append_direct" "xs'" in
-           let "r" = "append_direct1" "ys" in
-           let "r" = "x", "r" in
+           let ("x", "xs'") := "xs" in
+           let "append_direct1" := "append_direct" "xs'" in
+           let "r" := "append_direct1" "ys" in
+           let "r" := ("x", "r") in
            Inr "r"
        end }>.
 
 Example append_direct1 xs :=
-  <{ let "append_direct" = {append_direct} in
-     let "xs" = {xs} in
+  <{ let "append_direct" := append_direct in
+     let "xs" := xs in
      "append_direct" "xs" }>.
 
 Example append_direct2 xs ys :=
-  <{ let "append_direct" = {append_direct} in
-     let "xs" = {xs} in
-     let "ys" = {ys} in
-     let "append_direct1" = "append_direct" "xs" in
+  <{ let "append_direct" := append_direct in
+     let "xs" := xs in
+     let "ys" := ys in
+     let "append_direct1" := "append_direct" "xs" in
      "append_direct1" "ys" }>.
 
 Compute (eval_term 2 (append_direct1 (encode []))).
@@ -98,3 +96,57 @@ Compute (eval_term 3 (append_direct2 (encode [1]) (encode [2]))).
 
 Time Compute (eval_term 2000 (encode (sequence 0 1000))).
 Time Compute (eval_term_kont 2000 (encode (sequence 0 1000)) KNil).
+
+Example either :=
+  <{ fun "x" "y" => shift "k" let _ := "k" "x" in "k" "y" }>.
+
+Example ex2 :=
+  <{ let "either" := either in
+     let "result" := ref false in
+     let _ :=
+       reset
+         let "either1" := "either" true in
+         let "p" := "either1" false in
+         let "q" := "either1" false in
+         let "not_p" := not "p" in
+         let "not_q" := not "q" in
+         let "c1" := "p" || "q" in
+         let "c2" := "p" || "not_q" in
+         let "c3" := "not_p" || "not_q" in
+         let "r" := "c1" && "c2" in
+         let "r" := "r" && "c3" in
+         if "r" then "result" <- true else ()
+     in !"result" }>.
+
+Compute (eval_term 6 ex2).
+
+Example choice :=
+  <{ fun "xs" =>
+       shift "k"
+         let fix "go" "xs" :=
+           match "xs" with
+           | Inl _ => ()
+           | Inr "xs" =>
+               let ("x", "xs'") := "xs" in
+               let _ := "k" "x" in
+               "go" "xs'"
+           end
+         in "go" "xs" }>.
+
+Example sum xs :=
+  <{ let "choice" := choice in
+     let "xs" := xs in
+     let "result" := ref 0 in
+     let _ :=
+       reset
+         let "x" := "choice" "xs" in
+         let "r" := !"result" in
+         let "r" := "r" + "x" in
+         "result" <- "r"
+     in !"result" }>.
+
+Compute (run_term 3 (sum (encode []))).
+Compute (run_term 4 (sum (encode [1]))).
+Compute (run_term 5 (sum (encode [1; 2]))).
+
+Time Compute (run_term 1000 (sum (encode (sequence 0 500)))).
--- shift-reset/core/syntax_notation.v
+++ shift-reset/core/syntax_notation.v
@@ -9,9 +9,8 @@ Declare Custom Entry term.
 
 Notation "<{ t }>" := t (t custom term at level 99) : term_scope.
 Notation "( t )" := t (in custom term, t at level 99) : term_scope.
-Notation "{ t }" := t (in custom term, t constr) : term_scope.
 Notation "t" := t (in custom term at level 0, t constr at level 0) : term_scope.
-Notation "'begin' t 'end'" := t (in custom term, t custom term at level 99, only parsing) : term_scope.
+Notation "{ t }" := t (in custom term, t constr) : term_scope.
 
 Notation "a1 a2" :=
   (TApp a1 a2)
@@ -26,7 +25,7 @@ Notation "'if' a 'then' t1 'else' t2" :=
         t1 custom term,
         t2 custom term) : term_scope.
 
-Notation "'let' x '=' t1 'in' t2" :=
+Notation "'let' x := t1 'in' t2" :=
   (TLet t1 (T1 x t2))
     (in custom term at level 69,
         x at level 0,
@@ -34,7 +33,7 @@ Notation "'let' x '=' t1 'in' t2" :=
         t2 custom term,
         right associativity) : term_scope.
 
-Notation "'let' f x1 .. xn '=' t1 'in' t2" :=
+Notation "'let' f x1 .. xn := t1 'in' t2" :=
   (TLet (TFun (T1 x1 .. (TFun (T1 xn t1)) ..)) (T1 f t2))
     (in custom term at level 69,
         f, x1, xn at level 0,
@@ -42,7 +41,7 @@ Notation "'let' f x1 .. xn '=' t1 'in' t2" :=
         t2 custom term,
         right associativity) : term_scope.
 
-Notation "'let' 'rec' f x '=' t1 'in' t2" :=
+Notation "'let' 'fix' f x := t1 'in' t2" :=
   (TLet (TFix (T2 f x t1)) (T1 f t2))
     (in custom term at level 69,
         f, x at level 0,
@@ -50,7 +49,7 @@ Notation "'let' 'rec' f x '=' t1 'in' t2" :=
         t2 custom term,
         right associativity) : term_scope.
 
-Notation "'let' 'rec' f x1 x2 .. xn '=' t1 'in' t2" :=
+Notation "'let' 'fix' f x1 x2 .. xn := t1 'in' t2" :=
   (TLet (TFix (T2 f x1 (TFun (T1 x2 .. (TFun (T1 xn t1)) ..)))) (T1 f t2))
     (in custom term at level 69,
         f, x1, x2, xn at level 0,
@@ -58,7 +57,7 @@ Notation "'let' 'rec' f x1 x2 .. xn '=' t1 'in' t2" :=
         t2 custom term,
         right associativity) : term_scope.
 
-Notation "'shift' k '=>' t" :=
+Notation "'shift' k t" :=
   (TShift (T1 k t))
     (in custom term at level 69,
         k at level 0,
@@ -67,25 +66,26 @@ Notation "'shift' k '=>' t" :=
 Notation "'reset' t" :=
   (TReset t) (in custom term at level 69, t custom term) : term_scope.
 
-Notation "'fun' x1 .. xn '=>' t" :=
+Notation "'fun' x1 .. xn => t" :=
   (TFun (T1 x1 .. (TFun (T1 xn t)) ..))
     (in custom term at level 69,
         x1, xn at level 0,
         t custom term at level 99) : term_scope.
 
-Notation "'fix' f x '=>' t" :=
+Notation "'fix' f x := t" :=
   (TFix (T2 f x t))
     (in custom term at level 69,
         f, x at level 0,
         t custom term at level 99) : term_scope.
 
-Notation "'fix' f x1 x2 .. xn '=>' t" :=
+Notation "'fix' f x1 x2 .. xn := t" :=
   (TFix (T2 f x1 (TFun (T1 x2 .. (TFun (T1 xn t)) ..))))
     (in custom term at level 69,
         f, x1, x2, xn at level 0,
         t custom term at level 99) : term_scope.
 
 Notation "()" := AUnit (in custom term at level 0) : term_scope.
+Notation "'_'" := BAnon (in custom term at level 0) : term_scope.
 
 Notation "'not' a" :=
   (TPrim1 P1Not a) (in custom term at level 23, a custom term at level 0) : term_scope.
@@ -123,13 +123,37 @@ Notation "a1 = a2" :=
         a1 custom term,
         a2 custom term) : term_scope.
 
+Notation "a1 <> a2" :=
+  (TPrim2 P2Eq a1 a2)
+    (in custom term at level 50,
+        a1 custom term,
+        a2 custom term) : term_scope.
+
 Notation "a1 < a2" :=
   (TPrim2 P2Lt a1 a2)
     (in custom term at level 50,
         a1 custom term,
         a2 custom term) : term_scope.
 
-Notation "a1 := a2" :=
+Notation "a1 && a2" :=
+  (TPrim2 P2And a1 a2)
+    (in custom term at level 50,
+        a1 custom term,
+        a2 custom term) : term_scope.
+
+Notation "a1 || a2" :=
+  (TPrim2 P2Or a1 a2)
+    (in custom term at level 50,
+        a1 custom term,
+        a2 custom term) : term_scope.
+
+Notation "a1 'xor' a2" :=
+  (TPrim2 P2And a1 a2)
+    (in custom term at level 50,
+        a1 custom term,
+        a2 custom term) : term_scope.
+
+Notation "a1 <- a2" :=
   (TSet a1 a2)
     (in custom term at level 65,
         a1 custom term,
@@ -141,7 +165,14 @@ Notation "a1 , a2" :=
         a1 custom term,
         a2 custom term) : term_scope.
 
-Notation "'let' x1 , x2 '=' a 'in' t" :=
+Notation "'let' ( x1 , x2 ) := a 'in' t" :=
+  (TSplit a (T2 x1 x2 t))
+    (in custom term at level 69,
+        x1, x2 at level 0,
+        t custom term,
+        right associativity) : term_scope.
+
+Notation "'let' x1 , x2 := a 'in' t" :=
   (TSplit a (T2 x1 x2 t))
     (in custom term at level 69,
         x1, x2 at level 0,
@@ -154,7 +185,7 @@ Notation "'Inl' a" :=
 Notation "'Inr' a" :=
   (TInr a) (in custom term at level 23, a custom term at level 0) : term_scope.
 
-Notation "'match' a 'with' '|' 'Inl' x1 '=>' t1 '|' 'Inr' x2 '=>' t2 'end'" :=
+Notation "'match' a 'with' | 'Inl' x1 => t1 | 'Inr' x2 => t2 'end'" :=
   (TCase a (T1 x1 t1) (T1 x2 t2))
     (in custom term at level 69,
         a custom term,

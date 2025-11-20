638ff2e63ff8342cb6857c13d53ea42ffcae919d
VietAnh1010 <vietanh101003@gmail.com>
2025-11-20 23:42:34 +0800

    Add coercion for char

--- shift-reset/core/coerce.v
+++ shift-reset/core/coerce.v
@@ -1,4 +1,4 @@
-From Stdlib Require Import String Qcanon ZArith.
+From Stdlib Require Import Ascii String Qcanon ZArith.
 From shift_reset.core Require Import syntax tag var.
 
 Coercion Tag : string >-> tag.
@@ -9,6 +9,7 @@ Coercion PRecordVar : var >-> record_pattern.
 Coercion TVVar : var >-> val_term.
 Coercion TVInt : Z >-> val_term.
 Coercion TVFloat : Qc >-> val_term.
+Coercion TVChar : ascii >-> val_term.
 Coercion TVOp1 : op1 >-> Funclass.
 Coercion TVOp2 : op2 >-> Funclass.
 Coercion TVal : val_term >-> term.

ef8b531ff28278d19e61896ad0481cdd982f3d5c
VietAnh1010 <vietanh101003@gmail.com>
2025-12-19 01:09:49 +0800

    Change type vars on sum

--- shift-reset/lib/sum.v
+++ shift-reset/lib/sum.v
@@ -1,27 +1,27 @@
-Definition map {E A B} (f : A -> B) (s : E + A) : E + B :=
+Definition map {A B C} (f : B -> C) (s : A + B) : A + C :=
   match s with
   | inl e => inl e
   | inr x => inr (f x)
   end.
 
-Definition pure {E A} : A -> E + A := inr.
+Definition pure {A B} : B -> A + B := inr.
 
-Definition bind {E A B} (s : E + A) (f : A -> E + B) : E + B :=
+Definition bind {A B C} (s : A + B) (f : B -> A + C) : A + C :=
   match s with
   | inl e => inl e
   | inr x => f x
   end.
 
-Definition fold {E A B} (f : E -> B) (g : A -> B) (s : E + A) : B :=
+Definition fold {A B C} (f : A -> C) (g : B -> C) (s : A + B) : C :=
   match s with
   | inl e => f e
   | inr x => g x
   end.
 
 Lemma eq_dec :
-  forall {E A : Type}
-         (E_eq_dec : forall (x y : E), {x = y} + {x <> y})
+  forall {A B : Type}
          (A_eq_dec : forall (x y : A), {x = y} + {x <> y})
-         (s1 s2 : E + A),
+         (B_eq_dec : forall (x y : B), {x = y} + {x <> y})
+         (s1 s2 : A + B),
     {s1 = s2} + {s1 <> s2}.
 Proof. decide equality. Defined.

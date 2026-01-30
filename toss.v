3714797517d8227f94294d7e9fc78d22e4092518
VietAnh1010 <vietanh101003@gmail.com>
2026-01-30 17:13:12 +0800

    Investigating toss example

---
+++ toss.v
@@ -0,0 +1,57 @@
+From Stdlib Require Import List String QArith Qcanon ZArith.
+Import ListNotations.
+
+From shift_reset.core Require Import syntax syntax_notation coerce.
+From shift_reset.interpreter Require Import interpreter.
+
+Open Scope string_scope.
+Open Scope Z_scope.
+Open Scope term_scope.
+
+Example incr :=
+  <{ fun "x" => "x" <- !"x" + 1 }>.
+
+Example do_toss :=
+  <{ fun "x" =>
+       shift
+         (fun "k" =>
+            let _ := incr "x" in
+            let "r1" := "k" true in
+            let _ := incr "x" in
+            let "r2" := "k" false in
+            "r1" + "r2") }>.
+
+Example toss :=
+  <{ fun "x" =>
+       reset
+         let "r" := do_toss "x" in
+         if "r" then 1 else 0 }>.
+
+Example run_toss x :=
+  <{ let "x" := ref x in
+     let "r" := toss "x" in
+     !"x", "r" }>.
+
+Compute (eval_term 4 (run_toss 0)).
+
+Example do_toss_n :=
+  <{ fix "go" "args" :=
+        let ("n", "x") := "args" in
+        if "n" = 0 then true
+        else
+          let "r1" := do_toss "x" in
+          let "r2" := "go" ("n" - 1, "x") in
+          "r1" && "r2" }>.
+
+Example toss_n :=
+  <{ fun "args" =>
+       reset
+         let "r" := do_toss_n "args" in
+         if "r" then 1 else 0 }>.
+
+Example run_toss_n n x :=
+  <{ let "x" := ref x in
+     let "r" := toss_n (n, "x") in
+     !"x", "r" }>.
+
+Compute (eval_term 100 (run_toss_n 10 0)).

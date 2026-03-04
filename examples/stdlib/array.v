175da27e39aaf3836311cff2b64eab3d92150e80
VietAnh1010 <vietanh101003@gmail.com>
2026-03-04 18:46:17 +0800

    Add array and complex module

---
+++ examples/stdlib/array.v
@@ -0,0 +1,52 @@
+From Stdlib Require Import String ZArith.
+From shift_reset.core Require Import syntax syntax_notation coerce.
+
+Open Scope Z_scope.
+Open Scope string_scope.
+Open Scope term_scope.
+
+Example Array :=
+  <{ let "iter" "args" :=
+       let ("f", "a") := "args" in
+       let "l" := {TVBuiltin1 "array_length" "a"} in
+       for "i" from 0 upto "l" - 1 do "f" ("a".["i"])
+     in
+     let "iteri" "args" :=
+       let ("f", "a") := "args" in
+       let "l" := {TVBuiltin1 "array_length" "a"} in
+       for "i" from 0 upto "l" - 1 do "f" ("i", "a".["i"])
+     in
+     let "map" "args" :=
+       let ("f", "a") := "args" in
+       let "l" := {TVBuiltin1 "array_length" "a"} in
+       let "r" := {TVBuiltin2 "array_make" "l" 0} in
+       let _ :=
+         for "i" from 0 upto ("l" - 1) do
+           let "x" := "f" ("a".["i"]) in
+           "r".["i"] <- "x"
+       in
+       "r"
+     in
+     let "map_inplace" "args" :=
+       let ("f", "a") := "args" in
+       let "l" := {TVBuiltin1 "array_length" "a"} in
+       for "i" from 0 upto ("l" - 1) do
+         let "x" := "f" ("a".["i"]) in
+         "a".["i"] <- "x"
+     in
+     let "mapi" "args" :=
+       let ("f", "a") := "args" in
+       let "l" := {TVBuiltin1 "array_length" "a"} in
+       let "r" := {TVBuiltin2 "array_make" "l" 0} in
+       let _ :=
+         for "i" from 0 upto ("l" - 1) do
+           let "x" := "f" ("i", "a".["i"]) in
+           "r".["i"] <- "x"
+       in
+       "r"
+     in
+     `{ "iter"
+      ; "iteri"
+      ; "map"
+      ; "map_inplace"
+      ; "mapi" } }>.
---
+++ examples/stdlib/complex.v
@@ -0,0 +1,36 @@
+From Stdlib Require Import String.
+From shift_reset.core Require Import syntax syntax_notation coerce.
+
+Open Scope string_scope.
+Open Scope term_scope.
+
+Example Complex :=
+  <{ let "neg" "c" :=
+       let `{"re"; "im"} := "c" in
+       `{"re" := -"re"; "im" := -"im"}
+     in
+     let "conj" "c" :=
+       let `{"re"; "im"} := "c" in
+       `{"re"; "im" := -"im"}
+     in
+     let "add" "args" :=
+       let ("c1", "c2") := "args" in
+       let `{"re" := "re1"; "im" := "im1"} := "c1" in
+       let `{"re" := "re2"; "im" := "im2"} := "c2" in
+       `{"re" := ("re1" + "re2"); "im" := ("im1" + "im2")}
+     in
+     let "sub" "args" :=
+       let ("c1", "c2") := "args" in
+       let `{"re" := "re1"; "im" := "im1"} := "c1" in
+       let `{"re" := "re2"; "im" := "im2"} := "c2" in
+       `{"re" := ("re1" - "re2"); "im" := ("im1" - "im2")}
+     in
+     let "norm2" "c" :=
+       let `{"re"; "im"} := "c" in
+       "re" * "re" + "im" * "im"
+     in
+     `{ "neg"
+      ; "conj"
+      ; "add"
+      ; "sub"
+      ; "norm2" } }>.

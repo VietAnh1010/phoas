4f2f5eb35862028c6037c6833dc6b70d320cd711
VietAnh1010 <vietanh101003@gmail.com>
2026-03-13 03:01:06 +0800

    Fix indentation of for-loop (changing proof-general internal)

--- examples/stdlib/array.v
+++ examples/stdlib/array.v
@@ -7,34 +7,35 @@ Open Scope term_scope.
 
 Example Array :=
   <{ let "iter" ("f", "a") :=
-       for "i" from 0 upto {TVBuiltin1 "array_length" "a"} - 1 do "f" "a".["i"]
+       for "i" from 0 upto ({TVBuiltin1 "array_length" "a"} - 1) do
+         "f" "a".["i"]
      in
      let "iteri" ("f", "a") :=
-       for "i" from 0 upto {TVBuiltin1 "array_length" "a"} - 1 do "f" ("i", "a".["i"])
+       for "i" from 0 upto ({TVBuiltin1 "array_length" "a"} - 1) do
+         "f" ("i", "a".["i"])
      in
      let "map" ("f", "a") :=
        let "l" := {TVBuiltin1 "array_length" "a"} in
        let "r" := {TVBuiltin2 "array_make" "l" <{ () }>} in
        let _ :=
          for "i" from 0 upto ("l" - 1) do
-               let "x" := "f" "a".["i"] in
-               "r".["i"] <- "x"
+           let "x" := "f" "a".["i"] in
+           "r".["i"] <- "x"
        in
        "r"
      in
      let "map_inplace" ("f", "a") :=
-       let "l" := {TVBuiltin1 "array_length" "a"} in
-       for "i" from 0 upto ("l" - 1) do
-             let "x" := "f" "a".["i"] in
-             "a".["i"] <- "x"
+       for "i" from 0 upto ({TVBuiltin1 "array_length" "a"} - 1) do
+         let "x" := "f" "a".["i"] in
+         "a".["i"] <- "x"
      in
      let "mapi" ("f", "a") :=
        let "l" := {TVBuiltin1 "array_length" "a"} in
        let "r" := {TVBuiltin2 "array_make" "l" <{ () }>} in
        let _ :=
          for "i" from 0 upto ("l" - 1) do
-               let "x" := "f" ("i", "a".["i"]) in
-               "r".["i"] <- "x"
+           let "x" := "f" ("i", "a".["i"]) in
+           "r".["i"] <- "x"
        in
        "r"
      in

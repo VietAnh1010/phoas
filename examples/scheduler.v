dccab9720841a0895d1ba4b583df93afb5399cc6
VietAnh1010 <vietanh101003@gmail.com>
2026-03-17 18:56:12 +0800

    Add scheduler example

---
+++ examples/scheduler.v
@@ -0,0 +1,23 @@
+From Stdlib Require Import List String ZArith.
+From shift_reset.core Require Import syntax syntax_notation coerce val.
+From examples Require Import common.
+Import ListNotations.
+
+Open Scope Z_scope.
+Open Scope string_scope.
+Open Scope term_scope.
+
+Example fork := <{ fun "f" => perform `"Fork" "f" }>.
+Example yield := <{ fun _ => perform `"Yield" () }>.
+
+Example run :=
+  <{ fun "f" =>
+       let "q" := () in
+       let "enq" "k" := () in
+       let "deq" _ := () in
+       let fix "go" "f" :=
+         handle try "k" ();; (fun _ => "deq" ());;;
+         (fun (`"Yield" _) "k" => "enq" "k"; "deq" ());
+         (fun (`"Fork" "f") "k" => "enq" "k"; "go" "f")
+       in
+       "go" "f" }>.

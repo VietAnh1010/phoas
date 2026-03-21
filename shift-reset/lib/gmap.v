56813d3c6e3bbc4a0a9f6b743234445d45e112aa
VietAnh1010 <vietanh101003@gmail.com>
2026-03-21 21:37:43 +0800

    Lift new functions on pmap to gmap

--- shift-reset/lib/gmap.v
+++ shift-reset/lib/gmap.v
@@ -17,11 +17,20 @@ Arguments gmap_car {K A} _.
 
 Definition empty {K A} : gmap K A := GMap pmap.empty.
 
+Definition is_empty {K A} (mt : gmap K A) : bool :=
+  pmap.is_empty (gmap_car mt).
+
+Definition is_singleton {K A} (mt : gmap K A) : bool :=
+  pmap.is_singleton (gmap_car mt).
+
 Definition map {K A B} (f : A -> B) (mt : gmap K A) : gmap K B :=
   GMap (pmap.map f (gmap_car mt)).
 
 Definition filter_map {K A B} (f : A -> option B) (mt : gmap K A) : gmap K B :=
   GMap (pmap.filter_map f (gmap_car mt)).
+
+Definition cardinal {K A} (mt : gmap K A) : nat :=
+  pmap.cardinal (gmap_car mt).
 
 Module Make (K : IsoPositiveType).
   Definition find {A} (k : K.t) (mt : gmap K.t A) : option A :=
@@ -41,4 +50,13 @@ Module Make (K : IsoPositiveType).
 
   Definition update {A} (k : K.t) (f : option A -> option A) (mt : gmap K.t A) : gmap K.t A :=
     GMap (pmap.update (K.encode k) f (gmap_car mt)).
+
+  Definition mapi {A B} (f : K.t -> A -> B) (mt : gmap K.t A) : gmap K.t B :=
+    GMap (pmap.mapi (fun i => f (K.decode i)) (gmap_car mt)).
+
+  Definition filter_mapi {A B} (f : K.t -> A -> option B) (mt : gmap K.t A) : gmap K.t B :=
+    GMap (pmap.filter_mapi (fun i => f (K.decode i)) (gmap_car mt)).
+
+  Definition merge {A B C} (f : K.t -> option A -> option B -> option C) (mt1 : gmap K.t A) (mt2 : gmap K.t B) : gmap K.t C :=
+    GMap (pmap.merge (fun i => f (K.decode i)) (gmap_car mt1) (gmap_car mt2)).
 End Make.

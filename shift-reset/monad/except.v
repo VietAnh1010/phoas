ebf5ab62c5aa3b4d4de76d9ffadec0997836edb7
VietAnh1010 <vietanh101003@gmail.com>
2026-04-27 14:39:20 +0800

    Add alternative instance for except monad

--- shift-reset/monad/except.v
+++ shift-reset/monad/except.v
@@ -1,3 +1,5 @@
+From shift_reset.monad Require Import signatures.
+
 Record except (E A : Type) : Type := Except { run_except : E + A }.
 Definition t : Type -> Type -> Type := except.
 
@@ -93,3 +95,22 @@ Module ExceptNotations.
   Notation "let+ x := m 'in' k" := (map (fun x => k) m) (at level 100, x binder, right associativity) : except_scope.
   Notation "let* x := m 'in' k" := (bind m (fun x => k)) (at level 100, x binder, right associativity) : except_scope.
 End ExceptNotations.
+
+Module MakeAlternative (E : Monoid).
+  Definition empty {A} : except E.t A :=
+    Except (inl E.empty).
+
+  Definition combine {A} (m1 : except E.t A) (m2 : except E.t A) : except E.t A :=
+    Except match run_except m1 with
+      | inl e1 =>
+          match run_except m2 with
+          | inl e2 => inl (E.combine e1 e2)
+          | inr x => inr x
+          end
+      | inr x => inr x
+      end.
+
+  Module Notations.
+    Notation "m1 <|> m2" := (combine m1 m2) (at level 55, left associativity) : except_scope.
+  End Notations.
+End MakeAlternative.

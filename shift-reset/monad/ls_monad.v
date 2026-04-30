426e494c755b349d0b4f665d0d1fc2c631eb71ab
VietAnh1010 <vietanh101003@gmail.com>
2026-05-01 00:09:40 +0800

    Add ls_monad

---
+++ shift-reset/monad/ls_monad.v
@@ -0,0 +1,135 @@
+From Stdlib Require Import List.
+Import ListNotations.
+
+Local Unset Elimination Schemes.
+Local Unset Case Analysis Schemes.
+
+Inductive ls_monad (S A : Type) : Type :=
+| LSMonad : (S -> step S A * S) -> ls_monad S A
+with step (S A : Type) : Type :=
+| Nil : step S A
+| Cons : A -> ls_monad S A -> step S A.
+
+Definition t : Type -> Type -> Type := ls_monad.
+
+Arguments LSMonad {S A} _.
+Arguments Nil {S A}.
+Arguments Cons {S A} _ _.
+
+Definition run_ls_monad {S A} : ls_monad S A -> S -> step S A * S :=
+  fun '(LSMonad m) => m.
+
+Definition empty {S A} : ls_monad S A :=
+  LSMonad (fun s => (Nil, s)).
+
+Fixpoint combine {S A} (m1 : ls_monad S A) (m2 : ls_monad S A) : ls_monad S A :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m1 s in
+       match m with
+       | Nil => run_ls_monad m2 s
+       | Cons x m1' => (Cons x (combine m1' m2), s)
+       end).
+
+Definition pure {S A} (x : A) : ls_monad S A :=
+  LSMonad (fun s => (Cons x empty, s)).
+
+Fixpoint map {S A B} (f : A -> B) (m : ls_monad S A) : ls_monad S B :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons x m' => (Cons (f x) (map f m'), s)
+       end).
+
+Fixpoint map_const {S A B} (x : B) (m : ls_monad S A) : ls_monad S B :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons _ m' => (Cons x (map_const x m'), s)
+       end).
+
+Fixpoint apply {S A B} (m1 : ls_monad S (A -> B)) (m2 : ls_monad S A) : ls_monad S B :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m1 s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons f m1' => run_ls_monad (combine (map f m2) (apply m1' m2)) s
+       end).
+
+Fixpoint seq_left {S A B} (m1 : ls_monad S A) (m2 : ls_monad S B) : ls_monad S A :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m1 s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons x m1' => run_ls_monad (combine (map_const x m2) (seq_left m1' m2)) s
+       end).
+
+Fixpoint seq_right {S A B} (m1 : ls_monad S A) (m2 : ls_monad S B) : ls_monad S B :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m1 s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons _ m1' => run_ls_monad (combine m2 (seq_right m1' m2)) s
+       end).
+
+Fixpoint bind {S A B} (m : ls_monad S A) (f : A -> ls_monad S B) : ls_monad S B :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons x m' => run_ls_monad (combine (f x) (bind m' f)) s
+       end).
+
+Fixpoint join {S A} (m : ls_monad S (ls_monad S A)) : ls_monad S A :=
+  LSMonad
+    (fun s =>
+       let (m, s) := run_ls_monad m s in
+       match m with
+       | Nil => (Nil, s)
+       | Cons m m' => run_ls_monad (combine m (join m')) s
+       end).
+
+Fixpoint of_list {S A} (xs : list A) : ls_monad S A :=
+  match xs with
+  | [] => empty
+  | x :: xs' => LSMonad (fun s => (Cons x (of_list xs'), s))
+  end.
+
+Definition get {S} : ls_monad S S :=
+  LSMonad (fun s => (Cons s empty, s)).
+
+Definition put {S} (s : S) : ls_monad S unit :=
+  LSMonad (fun _ => (Cons tt empty, s)).
+
+Definition state {S A} (f : S -> A * S) : ls_monad S A :=
+  LSMonad (fun s => let (x, s) := f s in (Cons x empty, s)).
+
+Definition gets {S A} (f : S -> A) : ls_monad S A :=
+  LSMonad (fun s => (Cons (f s) empty, s)).
+
+Definition modify {S} (f : S -> S) : ls_monad S unit :=
+  LSMonad (fun s => (Cons tt empty, f s)).
+
+Module LSMonadNotations.
+  Declare Scope ls_monad_scope.
+  Delimit Scope ls_monad_scope with ls_monad.
+  Bind Scope ls_monad_scope with ls_monad.
+
+  Notation "f <$> m" := (map f m) (at level 65, right associativity) : ls_monad_scope.
+  Notation "x <$ m" := (map_const x m) (at level 65, right associativity) : ls_monad_scope.
+  Notation "m1 <*> m2" := (apply m1 m2) (at level 55, left associativity) : ls_monad_scope.
+  Notation "m1 <* m2" := (seq_left m1 m2) (at level 55, left associativity) : ls_monad_scope.
+  Notation "m1 *> m2" := (seq_right m1 m2) (at level 55, left associativity) : ls_monad_scope.
+  Notation "m >>= f" := (bind m f) (at level 50, left associativity) : ls_monad_scope.
+
+  Notation "let+ x := m 'in' k" := (map (fun x => k) m) (at level 100, x binder, right associativity) : ls_monad_scope.
+  Notation "let* x := m 'in' k" := (bind m (fun x => k)) (at level 100, x binder, right associativity) : ls_monad_scope.
+End LSMonadNotations.

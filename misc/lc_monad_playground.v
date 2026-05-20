6591b3f04271dfda27d659aa94805b0b9f7db099
VietAnh1010 <vietanh101003@gmail.com>
2026-05-20 12:51:16 +0800

    Update the playground

--- misc/lc_monad_playground.v
+++ misc/lc_monad_playground.v
@@ -1,5 +1,6 @@
-From Stdlib Require Import List Arith.
+From Stdlib Require Import List Arith FunctionalExtensionality.
 From shift_reset.monad Require Import lc_monad.
+From proofs.monad Require Import lc_monad.
 
 Import ListNotations.
 Import LCMonadNotations.
@@ -26,3 +27,33 @@ Example example' {R} : lc_monad R _ :=
 Check (run_lc_monad example [] List.cons).
 Compute (run_lc_monad example [] List.cons).
 Compute (run_lc_monad example' [] List.cons).
+
+Example example_lift {R} : lc_monad R _ :=
+  let* x := of_list [1; 2; 100] in
+  let* y := of_list [3; 4] in
+  callcc
+    (fun k =>
+       let* _ := if x =? 2 then k (0, 0, 0) else pure tt in
+       let* z := of_list [5; 6] in
+       pure (x, y, z)).
+
+Example example'_lift {R} : lc_monad R _ :=
+  let* x := of_list [1; 2; 100] in
+  let* y := of_list [3; 4] in
+  callcc'
+    (fun k =>
+       let* _ := if x =? 2 then k (0, 0, 0) else pure tt in
+       let* z := of_list [5; 6] in
+       pure (x, y, z)).
+
+Compute (run_lc_monad example_lift [] List.cons).
+Compute (run_lc_monad example'_lift [] List.cons).
+
+Lemma example'_eq_example'_lift {R} : @example' R = example'_lift.
+Proof.
+  unfold example', example'_lift.
+  rewrite -> callcc'_bind.
+  apply f_equal. apply functional_extensionality. intros x.
+  rewrite -> callcc'_bind.
+  reflexivity.
+Qed.
